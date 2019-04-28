aee253098fa69d5d8c27663f76a04c15d60105c2
loothy <gabrielbernal@hotmail.fr>
2019-04-28 07:38:27 +0200

    change 15

--- src/main/java/jenkins/TriTableau.java
+++ src/main/java/jenkins/TriTableau.java
@@ -1,14 +1,10 @@
 package jenkins;
 
-import java.io.IOException;
+
 
 public class TriTableau
 {
 	
-    public int Calcule(int cal) {
-    	int res = 5 + 5;
-    	
-    	return res + cal;
-    }
+  
 
 }
